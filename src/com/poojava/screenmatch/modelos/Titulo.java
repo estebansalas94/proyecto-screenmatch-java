4d4b794c646d3a55d546e85c7df14f43772aaabf
esteban salas <estebansalas0404@gmail.com>
2024-05-21 18:26:37 -0500

    Consumiendo la api de OMDb API en JAVA

--- src/com/poojava/screenmatch/modelos/Titulo.java
+++ src/com/poojava/screenmatch/modelos/Titulo.java
@@ -1,5 +1,8 @@
 package com.poojava.screenmatch.modelos;
 
+import com.google.gson.annotations.SerializedName;
+import com.poojava.screenmatch.excepcion.ErrorEnConversionDeDuracionException;
+
 public class Titulo implements Comparable<Titulo> {
     private String nombre;
     private int fechaDeLanzamiento;
@@ -11,6 +14,16 @@ public class Titulo implements Comparable<Titulo> {
     public Titulo(String nombre, int fechaDeLanzamiento) {
         this.nombre = nombre;
         this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public Titulo(TituloOmdb miTituloOmdb) {
+        this.nombre = miTituloOmdb.title();
+        this.fechaDeLanzamiento = Integer.valueOf(miTituloOmdb.year());
+
+        if (miTituloOmdb.runtime().contains("N/A")){
+            throw new ErrorEnConversionDeDuracionException("No pude convertir la duracion, porque contiene un N/A");
+        }
+        this.duracionEnMinutos = Integer.valueOf(miTituloOmdb.runtime().substring(0,3).replace(" ",""));
     }
 
     public String getNombre() {
@@ -67,5 +80,12 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public int compareTo(Titulo otroTitulo) {
         return this.getNombre().compareTo(otroTitulo.getNombre());
+    }
+
+    @Override
+    public String toString() {
+        return  "(Nombre: '" + nombre +
+                ", Fecha De Lanzamiento: " + fechaDeLanzamiento +
+                ", Duracion: " + duracionEnMinutos+")";
     }
 }
